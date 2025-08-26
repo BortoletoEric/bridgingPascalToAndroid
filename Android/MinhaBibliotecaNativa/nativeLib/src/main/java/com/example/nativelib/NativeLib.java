08a0e178ca82ed0fe4399d94c08ddb06943264d9
Eric Bortoleto <ericbortoleto_9@outlook.com>
2025-08-26 14:25:08 -0300

    feat(android): Primeira .aar gerada

---
+++ Android/MinhaBibliotecaNativa/nativeLib/src/main/java/com/example/nativelib/NativeLib.java
@@ -0,0 +1,14 @@
+package com.example.nativelib;
+
+public class NativeLib {
+    // Carrega a nossa biblioteca nativa ao iniciar a classe
+    static {
+        System.loadLibrary("meuprojetonativo"); // Note que o prefixo "lib" e o sufixo ".so" são omitidos
+    }
+
+    // Declaração da função nativa
+    public native int soma(int a, int b);
+    public native int subtrai(int a, int b);
+    public native int multiplica(int a, int b);
+    public native double divide(double a, double b);
+}
